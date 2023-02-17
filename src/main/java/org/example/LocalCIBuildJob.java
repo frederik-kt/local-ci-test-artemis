940d28e3f160f140500b71582da387bb84b9b85d
kutsch <frederik.kutsch@yahoo.com>
2023-02-17 17:18:24 +0100

    wait for results to be extracted before stopping the container

--- src/main/java/org/example/LocalCIBuildJob.java
+++ src/main/java/org/example/LocalCIBuildJob.java
@@ -12,7 +12,10 @@ import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -21,6 +24,7 @@ import javax.xml.stream.XMLStreamReader;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.io.IOUtils;
 
 public class LocalCIBuildJob {
 
@@ -66,17 +70,19 @@ public class LocalCIBuildJob {
                 .withEnv("ARTEMIS_BUILD_TOOL=" + buildTool.toString().toLowerCase(), "ARTEMIS_DEFAULT_BRANCH=main") // TODO: Replace with default branch for participation.
                 // Command to run when the container starts. This is the command that will be executed in the container's main process, which runs in the foreground and blocks the container from exiting until it finishes.
                 // It waits until the script that is running the tests (see below execCreateCmdResponse) is completed, which is running in the background and indicates termination by creating a file "script_completed.txt" in the root directory.
-                .withCmd("sh", "-c", "while [ ! -f /script_completed.txt ]; do sleep 0.5; done")
-                //.withCmd("tail", "-f", "/dev/null") // Activate for debugging purposes instead of the above command to get a running container that you can peek into using "docker exec -it <container-id> /bin/bash".
+                //.withCmd("sh", "-c", "while [ ! -f /results_extracted.txt ]; do sleep 0.5; done")
+                .withCmd("tail", "-f", "/dev/null") // Activate for debugging purposes instead of the above command to get a running container that you can peek into using "docker exec -it <container-id> /bin/bash".
                 .exec();
 
         try {
+            ZonedDateTime buildStartedDate = ZonedDateTime.now();
+
             // Start the container.
             dockerClient.startContainerCmd(container.getId()).exec();
 
             // The "sh script.sh" command specified here is run inside the container as an additional process. This command runs in the background, independent of the container's main process. The execution command can run concurrently with the main process.
             // Creates a script_completed file in the container's root directory when the script finishes. The main process is waiting for this file to appear and then stops the main process, thus stopping the container.
-            ExecCreateCmdResponse execCreateCmdResponse = dockerClient.execCreateCmd(container.getId()).withAttachStdout(true).withAttachStderr(true).withCmd("sh", "-c", "sh script.sh; touch /script_completed.txt").exec();
+            ExecCreateCmdResponse execCreateCmdResponse = dockerClient.execCreateCmd(container.getId()).withAttachStdout(true).withAttachStderr(true).withCmd("sh", "script.sh").exec();
 
             // Start the command and wait for it to complete.
             final CountDownLatch latch = new CountDownLatch(1);
@@ -111,158 +117,93 @@ public class LocalCIBuildJob {
                             .copyArchiveFromContainerCmd(container.getId(), testResultsPath)
                             .exec());
 
-            String resultsPath = "C:\\Users\\Kutsch\\IdeaProjects\\local-ci-test-artemis\\results\\";
+            TarArchiveInputStream assignmentRepoTarInputStream = new TarArchiveInputStream(dockerClient.copyArchiveFromContainerCmd(container.getId(), "/repositories/assignment-repository/.git/refs/heads/main").exec());
+            assignmentRepoTarInputStream.getNextTarEntry();
+            String assignmentRepoCommitHash = IOUtils.toString(assignmentRepoTarInputStream, StandardCharsets.UTF_8).replace("\n", "");
+            assignmentRepoTarInputStream.close();
 
-            // For test purpuses, extract the contents of the tar archive. TODO: Try to go through entries in the tar file directly.
-            TarArchiveEntry tarEntry;
-            while ((tarEntry = tarInputStream.getNextTarEntry()) != null) {
-                if (tarEntry.isDirectory()) {
-                    // create the directory if it doesn't exist
-                    new File(resultsPath + tarEntry.getName()).mkdirs();
-                } else {
-                    // create the file if it doesn't exist
-                    File outputFile = new File(resultsPath + tarEntry.getName());
-                    outputFile.createNewFile();
+            TarArchiveInputStream testRepoTarInputStream = new TarArchiveInputStream(dockerClient.copyArchiveFromContainerCmd(container.getId(), "/repositories/test-repository/.git/refs/heads/main").exec());
+            testRepoTarInputStream.getNextTarEntry();
+            String testRepoCommitHash = IOUtils.toString(testRepoTarInputStream, StandardCharsets.UTF_8).replace("\n", "");
+            testRepoTarInputStream.close();
 
-                    // write the contents of the tar entry to the file
-                    try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
-                        int count;
-                        byte[] data = new byte[2048];
-                        while ((count = tarInputStream.read(data)) != -1) {
-                            fileOutputStream.write(data, 0, count);
-                        }
-                    }
-                }
-            }
+            // Create a file "results_extracted.txt" in the root directory of the container to indicate that the test results have been extracted. The container's main process is waiting for this file to appear and then stops the main process, thus stopping and removing the container.
+            ExecCreateCmdResponse createResultsExtractedFileCmdResponse = dockerClient.execCreateCmd(container.getId()).withCmd("touch", "results_extracted.txt").exec();
+            dockerClient.execStartCmd(createResultsExtractedFileCmdResponse.getId()).exec(new ResultCallback.Adapter<>());
 
-            // Go through all xml result files created in /build/test-results/test, and extract data out of them.
-            boolean isBuildSuccessful = true;
+            ZonedDateTime buildCompletedDate = ZonedDateTime.now();
+            int duration = (int) ChronoUnit.SECONDS.between(buildStartedDate, buildCompletedDate);
+
             List<LocalCITestCaseDTO> failedTests = new ArrayList<>();
             List<LocalCITestCaseDTO> successfulTests = new ArrayList<>();
-
             List<String> timestamps = new ArrayList<>();
+            boolean isBuildSuccessful = true;
 
             XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+            TarArchiveEntry tarEntry;
+            while ((tarEntry = tarInputStream.getNextTarEntry()) != null) {
+                if (!tarEntry.isDirectory() && tarEntry.getName().startsWith(buildTool == BuildTool.GRADLE ? "test" : "surefire-reports" + "/TEST-") && tarEntry.getName().endsWith(".xml")) {
+                    // Read the contents of the tar entry as a string.
+                    String xmlString = IOUtils.toString(tarInputStream, StandardCharsets.UTF_8);
 
-            File dir = new File(resultsPath + "test");
-            File[] directoryListing = dir.listFiles();
-            if (directoryListing != null) {
-                for (File child : directoryListing) {
-                    if (!child.isDirectory() && child.getName().startsWith("TEST-") && child.getName().endsWith(".xml")) {
-                        // Create an XML stream reader for the entry.
-                        XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(new FileInputStream(child));
+                    // Create an XML stream reader for the string.
+                    XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(new StringReader(xmlString));
 
-                        // Move to the first start element.
-                        while (xmlStreamReader.hasNext() && !xmlStreamReader.isStartElement()) {
+                    // Move to the first start element.
+                    while (xmlStreamReader.hasNext() && !xmlStreamReader.isStartElement()) {
+                        xmlStreamReader.next();
+                    }
+
+                    // Check if the start element is the "testsuite" node.
+                    if (!xmlStreamReader.getLocalName().equals("testsuite")) {
+                        throw new IllegalStateException("Expected testsuite element, but got " + xmlStreamReader.getLocalName());
+                    }
+
+                    // Extract the timestamp attribute from the "testsuite" node.
+                    // TODO: Extract timestamp for maven.
+                    String timestamp = xmlStreamReader.getAttributeValue(null, "timestamp");
+                    timestamps.add(timestamp);
+
+                    // Go through all testcase nodes.
+                    while (xmlStreamReader.hasNext()) {
+                        xmlStreamReader.next();
+
+                        if (xmlStreamReader.isStartElement() && xmlStreamReader.getLocalName().equals("testcase")) {
+                            // Extract the name attribute from the "testcase" node.
+                            String name = xmlStreamReader.getAttributeValue(null, "name");
+
+                            // Check if there is a failure node inside the testcase node.
+                            // Call next() until there is an end element (no failure node exists inside the testcase node) or a start element (failure node exists inside the testcase node).
                             xmlStreamReader.next();
-                        }
-
-                        // Check if the start element is the "testsuite" node.
-                        if (!xmlStreamReader.getLocalName().equals("testsuite")) {
-                            throw new IllegalStateException("Expected testsuite element, but got " + xmlStreamReader.getLocalName());
-                        }
-
-                        // Extract the timestamp attribute from the "testsuite" node.
-                        String timestamp = xmlStreamReader.getAttributeValue(null, "timestamp");
-                        timestamps.add(timestamp);
-
-                        // Go through all testcase nodes.
-                        while (xmlStreamReader.hasNext()) {
-                            xmlStreamReader.next();
-
-                            if (xmlStreamReader.isStartElement() && xmlStreamReader.getLocalName().equals("testcase")) {
-                                // Extract the name attribute from the "testcase" node.
-                                String name = xmlStreamReader.getAttributeValue(null, "name");
-
-                                // Check if there is a failure node inside the testcase node.
-                                // Call next() until there is an end element (no failure node exists inside the testcase node) or a start element (failure node exists inside the testcase node).
+                            while (!(xmlStreamReader.isEndElement() || xmlStreamReader.isStartElement())) {
                                 xmlStreamReader.next();
-                                while (!(xmlStreamReader.isEndElement() || xmlStreamReader.isStartElement())) {
-                                    xmlStreamReader.next();
-                                }
-                                if (xmlStreamReader.isStartElement() && xmlStreamReader.getLocalName().equals("failure")) {
-                                    // Extract the message attribute from the "failure" node.
-                                    String message = xmlStreamReader.getAttributeValue(null, "message");
+                            }
+                            if (xmlStreamReader.isStartElement() && xmlStreamReader.getLocalName().equals("failure")) {
+                                // Extract the message attribute from the "failure" node.
+                                // TODO: Extract message for maven.
+                                String message = xmlStreamReader.getAttributeValue(null, "message");
 
-                                    // Add the failed test to the list of failed tests.
-                                    failedTests.add(new LocalCITestCaseDTO(name, List.of(message)));
+                                // Add the failed test to the list of failed tests.
+                                failedTests.add(new LocalCITestCaseDTO(name, message != null ? List.of(message) : null));
 
-                                    // If there is at least one test case with a failure node, the build is not successful.
-                                    isBuildSuccessful = false;
-                                } else {
-                                    // Add the successful test to the list of successful tests.
-                                    successfulTests.add(new LocalCITestCaseDTO(name, null));
-                                }
+                                // If there is at least one test case with a failure node, the build is not successful.
+                                isBuildSuccessful = false;
+                            } else {
+                                // Add the successful test to the list of successful tests.
+                                successfulTests.add(new LocalCITestCaseDTO(name, null));
                             }
                         }
-                        // Close the XML stream reader.
-                        xmlStreamReader.close();
                     }
+                    // Close the XML stream reader.
+                    xmlStreamReader.close();
                 }
             }
-
-//            TarArchiveEntry tarEntry;
-//            while ((tarEntry = tarInputStream.getNextTarEntry()) != null) {
-//                if (!tarEntry.isDirectory() && tarEntry.getName().startsWith(buildTool == BuildTool.GRADLE ? "test" : "surefire-reports" + "/TEST-") && tarEntry.getName().endsWith(".xml")) {
-//                    // Create an XML stream reader for the entry.
-//                    XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(tarInputStream);
-//
-//                    // Move to the first start element.
-//                    while (xmlStreamReader.hasNext() && !xmlStreamReader.isStartElement()) {
-//                        xmlStreamReader.next();
-//                    }
-//
-//                    // Check if the start element is the "testsuite" node.
-//                    if (!xmlStreamReader.getLocalName().equals("testsuite")) {
-//                        throw new IllegalStateException("Expected testsuite element, but got " + xmlStreamReader.getLocalName());
-//                    }
-//
-//                    // Extract the timestamp attribute from the "testsuite" node.
-//                    String timestamp = xmlStreamReader.getAttributeValue(null, "timestamp");
-//                    timestamps.add(timestamp);
-//
-//                    // Go through all testcase nodes.
-//                    while (xmlStreamReader.hasNext()) {
-//                        xmlStreamReader.next();
-//
-//                        if (xmlStreamReader.isStartElement() && xmlStreamReader.getLocalName().equals("testcase")) {
-//                            // Extract the name attribute from the "testcase" node.
-//                            String name = xmlStreamReader.getAttributeValue(null, "name");
-//
-//                            // Check if there is a failure node inside the testcase node.
-//                            if (xmlStreamReader.hasNext() && xmlStreamReader.isStartElement() && xmlStreamReader.getLocalName().equals("failure")) {
-//                                // Extract the message attribute from the "failure" node.
-//                                String message = xmlStreamReader.getAttributeValue(null, "message");
-//
-//                                // Add the failed test to the list of failed tests.
-//                                failedTests.add(new LocalCITestCaseDTO(name, List.of(message)));
-//
-//                                // If there is at least one test case with a failure node, the build is not successful.
-//                                isBuildSuccessful = false;
-//                            } else {
-//                                // Add the successful test to the list of successful tests.
-//                                successfulTests.add(new LocalCITestCaseDTO(name, null));
-//                            }
-//                        }
-//                    }
-//                    // Close the XML stream reader.
-//                    xmlStreamReader.close();
-//                }
-//            }
-//
-//            // Close the TAR archive input stream.
-//            tarInputStream.close();
 
             // Find the latest timestamp found in the test results.
 
-            // The commit hash of the assignment repository is located in /repositories/assignment-repository/.git/refs/heads/main.
-            String commitHashAssignmentRepository = null;
+            LocalCIBuildJobDTO buildJob = new LocalCIBuildJobDTO(isBuildSuccessful, assignmentRepoCommitHash, testRepoCommitHash, failedTests, successfulTests);
 
-            // The commit hash of the test repository is located in /repositories/test-repository/.git/refs/heads/main.
-            String commitHashTestRepository = null;
-
-            LocalCIBuildJobDTO buildJob = new LocalCIBuildJobDTO(isBuildSuccessful, commitHashAssignmentRepository, commitHashTestRepository, failedTests, successfulTests);
-
+            System.out.println(duration);
             System.out.println(timestamps);
             System.out.println(buildJob);
 
